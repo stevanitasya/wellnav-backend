7bd4843399f2484079dd2e2901c62492d776ffaf
stevanitasya <stevanitasya499@gmail.com>
2024-06-18 10:28:50 +0700

    server.js put a new code

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'Public')));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use('/api/users', userRoutes);
