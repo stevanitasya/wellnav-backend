04938698d5f710eb5d86b962210429aa7a7f0193
stevanitasya <stevanitasya499@gmail.com>
2024-06-18 22:41:57 +0700

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
+// middleware/auth.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-module.exports = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  
+const auth = async (req, res, next) => {
   try {
+    const token = req.header('Authorization').replace('Bearer ', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    const user = await User.findOne({ _id: decoded.id, 'tokens.token': token });
 
     if (!user) {
       throw new Error();
@@ -15,7 +15,9 @@ module.exports = async (req, res, next) => {
     req.token = token;
     req.user = user;
     next();
-  } catch (e) {
+  } catch (error) {
     res.status(401).send({ error: 'Please authenticate.' });
   }
 };
+
+module.exports = auth;
