14710d4f9a19f51aa5dd233f3c6f45f142e48cfd
stevanitasya <stevanitasya499@gmail.com>
2024-06-18 20:40:55 +0700

    watertrackingroutes error

--- routes/waterTrackingRoutes.js
+++ routes/waterTrackingRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const waterTrackingController = require('../controllers/waterTrackingController');
+const waterController = require('../controllers/waterTrackingController');
 const auth = require('../middleware/auth');
 
-router.post('/', auth, waterTrackingController.addWaterLog);
-router.get('/today', auth, waterTrackingController.getTodayWaterLogs);
+router.post('/', auth, waterController.addWaterLog);
+router.get('/today', auth, waterController.getTodayWaterLogs);
 
 module.exports = router;
