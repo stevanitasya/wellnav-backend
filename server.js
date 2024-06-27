915d1340baa895e06e114bcca9792b07356e78d9
stevanitasya <stevanitasya499@gmail.com>
2024-06-27 23:56:22 +0700

    change port

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
