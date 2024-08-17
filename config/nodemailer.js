a9bd2f2b1397ceb5fa69be2cc98c4445dda6b294
stevanitasya <stevanitasya499@gmail.com>
2024-08-17 14:59:40 +0700

    change link verify - stev

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -11,11 +11,12 @@ const sendVerificationEmail = async (user, token) => {
     },
   });
 
+  const vercelUrl = process.env.VERCEL_URL || 'http://localhost:5001'
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: user.email,
     subject: 'Email Verification',
-    text: `Please verify your email by clicking on the following link: http://localhost:5001/api/users/verify-email?token=${token}`,
+    text: `Please verify your email by clicking on the following link: ${vercelUrl}?token=${token}`,
   };
 
   try {
